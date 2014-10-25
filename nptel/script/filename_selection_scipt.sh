3247bb00333667cc53d6a2a17990dbc8582cd1d0
arajparaj <arajparaj@gmail.com>
2014-10-25 22:05:40 +0530

    script to create the thumbnail

--- nptel/script/filename_selection_scipt.sh
+++
@@ -1,5 +0,0 @@
-list=`cat $1`
-for ele in $list
-do
-	cat export2.csv | grep $ele
-done
--- nptel/script/scriptall.sh
+++
@@ -1,6 +0,0 @@
-cd ../web
-list=`ls *.html`
-for ele in $list
-do
-	tail -n +63 $ele >> ../script/final
-done
