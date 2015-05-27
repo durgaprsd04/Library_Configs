3ac3a26e025583fc2e4b12c4802ff7a0d4b28e39
shrimadhav <ssl-16@shrimadhav.in>
2015-05-27 12:53:12 +0530

    deleted the unwanted files. need to comment and indent the code.

---
+++ nptel/NPTEL/video-play.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>NPTEL Video Repo</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<link href="js/video-js/video-js.css" rel="stylesheet">
+<script src="js/video-js/video.js"></script>
+
+<script>
+  videojs.options.flash.swf = "js/video-js/video-js.swf"
+</script>
+
+    <div class="wrap">
+        <div class="sidebar">
+            <header>
+                <div class="logo">
+                    <a href="index.html" title="Mad"></a>
+                </div>
+                <div class="clearFloat"></div>
+               <!-- <div class="search-box">
+                    <form action="#">
+                        <input type="text">
+                        <input type="submit" value="">
+                    </form>
+                </div> -->
+                <div class="clearFloat"></div>
+            </header>
+            <div class="widget">
+                <ul>
+                            <li><a href="index.html">All&nbsp;Categories</a></li>
+                            <li><a href="index.html#AerospaceEngineering">Aerospace&nbsp;Engineering</a></li>
+                            <li><a href="index.html#Basiccourses(Sem1and2)">Basic&nbsp;courses(Sem&nbsp;1&nbsp;and&nbsp;2)</a></li>
+                            <li><a href="index.html#Biotechnology">Biotechnology</a></li>
+                            <li><a href="index.html#ChemicalEngineering">Chemical&nbsp;Engineering</a></li>
+                            <li><a href="index.html#ChemistryAndBiochemistry">Chemistry&nbsp;And&nbsp;Biochemistry</a></li>
+                            <li><a href="index.html#CivilEngineering">Civil&nbsp;Engineering</a></li>
+                            <li><a href="index.html#ComputerScienceAndEngineering">Computer&nbsp;Science&nbsp;And&nbsp;Engineering</a></li>
+                            <li><a href="index.html#ElectricalEngineering">Electrical&nbsp;Engineering</a></li>
+                            <li><a href="index.html#ElectronicsAndCommunicationEngineering">Electronics&nbsp;And&nbsp;Communication&nbsp;Engineering</a></li>
+                            <li><a href="index.html#EngineeringDesign">Engineering&nbsp;Design</a></li>
+                            <li><a href="index.html#HumanitiesandSocialSciences">Humanities&nbsp;and&nbsp;Social&nbsp;Sciences</a></li>
+                            <li><a href="index.html#Management">Management</a></li>
+                            <li><a href="index.html#Mathematics">Mathematics</a></li>
+                            <li><a href="index.html#MechanicalEngineering">Mechanical&nbsp;Engineering</a></li>
+                            <li><a href="index.html#MetallurgyandMaterialScience">Metallurgy&nbsp;and&nbsp;Material&nbsp;Science</a></li>
+                            <li><a href="index.html#OceanEngineering">Ocean&nbsp;Engineering</a></li>
+                            <li><a href="index.html#Physics">Physics</a></li>
+                            <li><a href="index.html#TextileEngineering">Textile&nbsp;Engineering</a></li>
+                            <li><a href="index.html#Miscellanious">Miscellanious</a></li>
+                </ul>
+            </div>
+            <div class="clearFloat"></div>
+        </div>
+        <div class="content">
+            <div class="clearFloat"></div>
+            <div class="part">
+                <h1><a href="#">Video Player</a></h1>
+                <div class="player">
+                    <video id="example_video_1" class="video-js vjs-default-skin"
+                        controls preload="auto" width="640" height="264"
+                      poster="http://video-js.zencoder.com/oceans-clip.png"
+                        data-setup='{"example_option":true}'>
+						<?php
+//							$dir = htmlspecialchars($_GET['vid']);
+							//echo $dir;
+							//$files = array_diff(scandir($directory), array('..', '.'));
+//							$dh  = opendir($dir);
+//							while (false !== ($filename = readdir($dh))) {
+//					echo "<source src=\"http://192.168.240.11".$dir."/".$filename."\" type=\'video/mp4\' />";
+//							}
+							//print_r($files);
+						?>
+
+                        <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+                    </video>
+                </div>
+                <h2>Other Videos</h2>
+
+
+      <div class="clearFloat"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
